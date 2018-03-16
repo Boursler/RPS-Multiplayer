b3d637e8a15bcb264802b0abf901b09ce1e97d49
Briana Oursler <boursler@blackhole.localdomain>
2018-03-16 14:21:49 -0700

    Remove one of the player area's from html as each player will see just their own, add Players to firebase database and increment # as players are added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,23 +8,23 @@ var config = {
 	storageBucket: "rps-multiplayer-8bfc4.appspot.com",
 	messagingSenderId: "297548366834"
 };
+
 firebase.initializeApp(config);
 var database = firebase.database();
 function Player(name) {
 	this.name = name;
 	this.wins = 0
 	this.losses = 0;
-	this.guess;
+	this.play;
 }
-
-
+var player1;
+var player2;
+var childrenCount;
 var rpsGame = {
 	R: 0,
 	P: 1,
 	S: 2,
 
-	player1: new Player("player1"),
-	player2: new Player("player2"),
 	play: function () {
 		var outcome = (rpsGame.player2.guess - rpsGame.player1.guess + 3) % 3;
 		if (outcome === 1) {
@@ -40,3 +40,53 @@ var rpsGame = {
 		}
 	}
 }
+
+function assignPlayer() {
+	event.preventDefault();
+	var name;
+	name = $("#player1Name").val();
+	console.log("adding name" + name);
+	player1 = new Player(name)
+	database.ref('players/' + nextId).set(player1);
+}
+
+function makePlay() {
+	//check if there has been a play
+	//if no:add play based on ID
+	//if yes: don't allow play
+}
+
+$("#add-player1").click(assignPlayer);
+$(".play").click(makePlay);
+
+
+
+database.ref().on("value", function (data) {
+	childrenCount = data.numChildren();
+	var player_list;
+	if (data.val() !== null) {
+		player_list = data.val().players;
+	}
+	console.log(player_list);
+	if (player_list)
+		nextId = player_list.length;
+	else
+		nextId = 1;
+
+	console.log("Next id: " + nextId);
+	console.log(JSON.stringify(data))
+	console.log(childrenCount + "numPlayers");
+	if (childrenCount === 1) {
+		console.log(data.val());
+
+	}
+	else if (childrenCount === 2) {
+		console.log(data.val());
+	}
+	else {
+		console.log("users full");
+	}
+
+}, function (error) {
+	//handle error
+});
