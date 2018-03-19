90def6e3a50b0f7a457f91525a0cc9c1dccaf3b0
Briana Oursler <boursler@blackhole.localdomain>
2018-03-18 17:43:27 -0700

    Rearchitected code; now it works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,65 +16,106 @@ function Player(name, playerId) {
 	this.name = name;
 	this.wins = 0
 	this.losses = 0;
-	this.play;
+	this.play = "";
 }
 
-var player;
-var childrenCount;
-var playerId;
+
 var rpsGame = {
 	R: 0,
 	P: 1,
 	S: 2,
+	playerCount: 0,
+	player: "",
+	opponent: "",
+	nextId: 3,
 
 	play: function () {
-		//TODO get these variables from somewhere
-		var player1, player2;
-
-		var outcome = (player2.play - player1.play + 3) % 3;
+		var outcome = (parseInt(this.opponent.play) - parseInt(this.player.play) + 3) % 3;
 		if (outcome === 1) {
-			player2.wins++;
-			player1.losses++;
+			//submit changes to database
+			this.player.losses++;
+
 		}
 		else if (outcome === 2) {
-			player1.wins++;
-			player2.losses++;
+			this.player.wins++;
+			//submit changes to database
+
 		}
 		else if (outcome === 0) {
 			//game is tied
 		}
+		this.player.play = "";
+		database.ref("players/" + this.player.playerId).set(this.player);
+
+	},
+	submitPlay: function () {
+		//check if there has been a play
+		//if no:add play based on ID
+		//if yes: don't allow play
+		rpsGame.player.play = $(this).attr("value");
+		database.ref("players/" + rpsGame.player.playerId + "/play").set(rpsGame.player.play);
+	},
+	assignPlayer: function () {
+		event.preventDefault();
+		var name;
+		playerId = rpsGame.nextId;
+		name = $("#player1Name").val();
+		rpsGame.player = new Player(name, playerId)
+		database.ref('players/' + playerId).set(rpsGame.player);
+		database.ref("players/" + playerId.toString()).onDisconnect().remove();
+	},
+	updateGameState: function (data) {
+		var player_list;
+		if (data.val() !== null) {
+			player_list = data.val().players;
+			rpsGame.playerCount = player_list.length - 1;
+			if (rpsGame.playerCount === 2) {
+				rpsGame.nextId = 3;
+			}
+			else if (rpsGame.playerCount === 0) {
+				rpsGame.nextId = 1;
+			}
+			else if (rpsGame.playerCount && player_list[1].playerId === 1) {
+				rpsGame.nextId = 2;
+			}
+			else {
+				rpsGame.nextId = 1;
+			}
+		}
+		else {
+			rpsGame.playerCount = 0;
+			rpsGame.nextId = 1;
+		}
+		if (rpsGame.player) {
+			if (rpsGame.playerCount === 2) {
+				player_list[0] = player_list[2];
+				rpsGame.opponent = player_list[(rpsGame.player.playerId + 1) % 2];
+
+
+			}
+			else if (rpsGame.playerCount === 1) {
+				rpsGame.opponent = "";
+			}
+		}
 	}
-}
-
-function assignPlayer() {
-	event.preventDefault();
-	var name;
-	playerId = nextId;
-	name = $("#player1Name").val();
-	console.log("adding name" + name);
-	player = new Player(name, playerId)
-	database.ref('players/' + playerId).set(player);
-	// sessionStorage.setItem("playerName", player.name);
-	// sessionStorage.setItem("playerWins", player.wins);
-	// sessionStorage.setItem("playerLosses", player.losses);
-	// sessionStorage.setItem("playerId", playerId);
-	database.ref("players/" + playerId.toString()).onDisconnect().remove();
-	// console.log(sessionStorage.getItem("playerName"));
 
 }
+
+
 
 function drawScreen() {
-	// console.log(sessionStorage.getItem("playerName"));
-	// if (sessionStorage.getItem("playerName") !== null) {
-	if (player) {
+	$("#add-player1").off("click");
+
+	if (rpsGame.nextId < 3 && rpsGame.player === "") {
+		$("#add-player1").click(rpsGame.assignPlayer);
+	}
+	if (rpsGame.player) {
 		$("#player1").hide();
 		$("#playerName").show();
-		$("#playerName").text(player.name);
-		$("#player1Wins").show().text(player.wins);
-		$("#player1Losses").show().text(player.losses);
-		// $("#playerName").text(sessionStorage.getItem("playerName"));
-		// $("#player1Wins").show().text(sessionStorage.getItem("playerWins"));
-		// $("#player1Losses").show().text(sessionStorage.getItem("playerLosses"));
+		$("#playerName").text(rpsGame.player.name);
+		$("#player1Wins").show().text(rpsGame.player.wins);
+		$("#player1Losses").show().text(rpsGame.player.losses);
+
 	}
 	else {
 		$("#player1").show();
@@ -83,50 +124,17 @@ function drawScreen() {
 		$("#player1Losses").hide();
 	}
 }
-
-function makePlay() {
-	//check if there has been a play
-	//if no:add play based on ID
-	//if yes: don't allow play
-
-	database.ref("players/" + player.playerId + "/play").set($(this).attr("id"));
-
-
-}
-
-
-$(".play").click(makePlay);
-
-
+$(".play").click(rpsGame.submitPlay);
 
 database.ref().on("value", function (data) {
-	childrenCount = data.numChildren();
-	console.log("changed:");
-	var player_list;
-	if (data.val() !== null) {
-		player_list = data.val().players;
+	rpsGame.updateGameState(data);
+
+	if (rpsGame.opponent !== "" && rpsGame.player !== "") {
+		if (rpsGame.opponent.play !== "" && rpsGame.player.play !== "") {
+			rpsGame.play();
+		}
 	}
-
-	console.log(player_list);
-	if (player_list) {
-		nextId = player_list.length;
-
-
-	}
-	else
-		nextId = 1;
-
-	$("#add-player1").off("click");
-
-
-	if (nextId < 3 && player === undefined) {
-		console.log("huh");
-		//prevent third player from joining
-		$("#add-player1").click(assignPlayer);
-	}
-
 	drawScreen();
-	console.log("change end");
 }, function (error) {
 	//handle error
 });
